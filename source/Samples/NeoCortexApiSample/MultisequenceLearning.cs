517d101b3849114c849a0e0ddad0bd2a740f3c11
Faiz <faizmohammedkhan7@gmail.com>
2023-02-16 21:24:34 +0100

    code refactor, fixed ambiguous issue

--- source/Samples/NeoCortexApiSample/MultisequenceLearning.cs
+++ source/Samples/NeoCortexApiSample/MultisequenceLearning.cs
@@ -246,7 +246,7 @@ namespace NeoCortexApiSample
                             Debug.WriteLine($"Match. Actual value: {key} - Predicted value: {lastPredictedValues.FirstOrDefault(key)}.");
                         }
                         else
-                            Debug.WriteLine($"Missmatch! Actual value: {key} - Predicted values: {String.Join(',', lastPredictedValues)}");
+                            Debug.WriteLine($"Missmatch! Actual value: {key} - Predicted values: {string.Join(',', lastPredictedValues)}");
 
                         if (lyrOut.PredictiveCells.Count > 0)
                         {
