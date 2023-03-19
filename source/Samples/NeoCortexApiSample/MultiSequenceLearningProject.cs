38c0d811edc302142a52d5036891c753057b9cac
MOHAN SAI RAM SARNALA <116576708+mohansairam117@users.noreply.github.com>
2023-03-19 18:23:54 +0100

    code update

--- source/Samples/NeoCortexApiSample/MultiSequenceLearningProject.cs
+++ source/Samples/NeoCortexApiSample/MultiSequenceLearningProject.cs
@@ -26,10 +26,7 @@ namespace MultiSequencePrediction
 
             //Code for reading the learning sequences from .txt file. The file has n rows which have numbers seperated by commas.
             sequences = ReadSequences(_trainSequencePath);
-            //sequences.Add("S1", new List<double>(new double[] { 0.0, 1.0, 2.0, 3.0, 4.0, 2.0, 5.0 }));
-            //sequences.Add("S2", new List<double>(new double[] { 8.0, 1.0, 2.0, 9.0, 10.0, 7.0, 11.00 }));
-            //sequences.Add("S3", new List<double>(new double[] { 8.0, 1.0, 2.0, 9.0, 15.0, 18.0, 17.00 }));
-
+           
             MultiSequenceLearning newExperiment = new MultiSequenceLearning();
             
             var predictor = newExperiment.Run(sequences, _encoderSettings);
